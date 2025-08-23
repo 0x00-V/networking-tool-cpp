08ccaebd5432436e4dc22207c5c3c8dee167e4fb
Daniel Thomas <daniel@null.com>
2025-08-24 00:20:57 +0100

    Logic fix

--- update-main.cpp
+++ update-main.cpp
@@ -1,107 +1,127 @@
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <errno.h>
-#include <iostream>
-#include <string.h>
-#include <sstream>
-#include <sys/select.h>
-#include <netdb.h>
+    #include <sys/socket.h>
+    #include <netinet/in.h>
+    #include <arpa/inet.h>
+    #include <fcntl.h>
+    #include <unistd.h>
+    #include <errno.h>
+    #include <iostream>
+    #include <string.h>
+    #include <sstream>
+    #include <sys/select.h>
+    #include <netdb.h>
 
 
-// --help (We can also use "-h" for shorthand)
-void help(std::string program_name)
-{
-    std::cout << "Daniel's Port Scanner";
-    std::cout << "\n\nSupported CLI Args:\n";
-    std::cout << "(-t) --target=\n(-p) --port=\n(-pr) --port-range=\nMore options coming soon!\n";
-    std::cout << "Example Usage:\n" << program_name << "-t=google.com -pr=1-65535\n";
-}
-
-
-// --target=0.0.0.0, or --target=google.com (We can also use "-t" for shorthand)
-void target(const std::string &value) {
-    std::cout << "Target: " << value << "\n";
-}
-
-
-// --port=80 (We can also use "-p" for shorthand)
-void port(int value){
-    std::cout << "Port: " << value << "\n";
-}
-
-// --port-range=1-65535 (We can also use "-pr" for shorthand)
-void port_range(int start, int end) {
-    std::cout << "Port range: " << start << "-" << end << "\n";
-}
-
-
-// Port scanning function
-void port_scan()
-{
-     std::cout << "Work in progress";
-}
-
-
-
-
-
-
-
-int main(int argc, char *argv[]) {
-    if(argc == 1)
+    // --help (We can also use "-h" for shorthand)
+    void help(std::string program_name)
     {
-        std::cout << "Incorrect Usage.\n" << "(Hint: Try \"" << argv[0] << " -h (or --help)\")\n";
+        std::cout << "Daniel's Port Scanner";
+        std::cout << "\n\nSupported CLI Args:\n";
+        std::cout << "(-t) --target=\n(-p) --port=\n(-pr) --port-range=\nMore options coming soon!\n";
+        std::cout << "Example Usage:\n" << program_name << "-t=google.com -pr=1-65535\n";
     }
-    for(int i = 1; i < argc; i++) {
-        std::string arg = argv[i];
 
-        if(arg == "--help" || arg == "-h") {
-            help(argv[0]);
-        }else if(arg.rfind("--target=", 0) == 0) {
-            std::string value = arg.substr(9);
-            target(value);
-        }else if(arg.rfind("-t=", 0) == 0) {
-            std::string value = arg.substr(3);
-            target(value);
-        }else if(arg.rfind("--port=", 0) == 0) {
-            int value = std::stoi(arg.substr(7));
-            port(value);
-        }else if(arg.rfind("-p=", 0) == 0) {
-            int value = std::stoi(arg.substr(3));
-            port(value);
-        }else if(arg.rfind("--port-range=", 0) == 0) {
-            std::string range = arg.substr(13);
-            size_t dash = range.find('-');
-            if(dash != std::string::npos) {
-                int start = std::stoi(range.substr(0, dash));
-                int end = std::stoi(range.substr(dash+1));
-                port_range(start, end);
+    void processTarget(std::string &target_address)
+    {
+        struct addrinfo hints{}, *res;
+        hints.ai_family = AF_INET;
+        hints.ai_socktype = SOCK_STREAM;
+
+        int status = getaddrinfo(target_address.c_str(), nullptr, &hints, &res);
+        if(status != 0)
+        {
+            freeaddrinfo(res);
+            std::cout << "getaddrinfo error: " << gai_strerror(status) << "\n";
+            std::cout << "Invalid Host Name (Please exclude http/https).\n";
+            exit(EXIT_FAILURE);
+        }
+
+        char ipstr[INET_ADDRSTRLEN];
+        struct sockaddr_in *ipv4 = (struct sockaddr_in *)res->ai_addr;
+        inet_ntop(AF_INET, &(ipv4->sin_addr), ipstr, sizeof(ipstr));
+        target_address = ipstr;
+    }
+
+
+    int main(int argc, char *argv[]) {
+        if(argc == 1)
+        {
+            std::cout << "[E-NP] Please provide valid options. \n(Hint: Try \"" << argv[0] << " -h (or --help)\")\n";
+            return 0;
+        }
+
+        int target_port = 0;
+        std::string target_address = "not set";
+        std::string port_range = "0";
+
+
+        for(int i = 1; i < argc; i++) {
+            std::string arg = argv[i];
+
+            if(arg == "--help" || arg == "-h") {
+                help(argv[0]);
+                return 0;
+            }else if(arg.rfind("--target=", 0) == 0) {
+                target_address = arg.substr(9);
+            }else if(arg.rfind("-t=", 0) == 0) {
+                target_address = arg.substr(3);
+            }else if(arg.rfind("--port=", 0) == 0) {
+                try {
+                    target_port = std::stoi(arg.substr(7));
+                } catch (const std::invalid_argument& e) {
+                    std::cout << "[E-STOI-P] Please enter a numeric value for the port.\n";
+                    return 0;
+                } catch (const std::out_of_range& e) {
+                    std::cout << "[E-STOI-P] Port value is out of range.\n";
+                    return 0;
+                }
+            } else if(arg.rfind("-p=", 0) == 0) {
+                try {
+                    target_port = std::stoi(arg.substr(3));
+                } catch (const std::invalid_argument& e) {
+                    std::cout << "[E-STOI-P] Please enter a numeric value for the port.\n";
+                    return 0;
+                } catch (const std::out_of_range& e) {
+                    std::cout << "[E-STOI-P] Port value is out of range.\n";
+                    return 0;
+                }
+            }else if(arg.rfind("--port-range=", 0) == 0) {
+                port_range = arg.substr(13);
+                size_t dash = port_range.find('-');
+                if(dash == std::string::npos) { std::cout << "Invalid port range: " << port_range << "\n"; }
+            }else if(arg.rfind("-pr=", 0) == 0) {
+                port_range = arg.substr(4);
+                size_t dash = port_range.find('-');
+                if(dash == std::string::npos) { std::cout << "Invalid port range: " << port_range << "\n"; }
             }else {
-                std::cerr << "Invalid port range: " << range << "\n";
+                std::cout << "[E-UKA] Unknown argument: " << argv[i] << "\n(Hint: Try \"" << argv[0] << " -h (or --help)\")\n";
+                return 0;
             }
-        }else if(arg.rfind("-pr=", 0) == 0) {
-            std::string range = arg.substr(4);
-            size_t dash = range.find('-');
-            if(dash != std::string::npos) {
-                int start = std::stoi(range.substr(0, dash));
-                int end = std::stoi(range.substr(dash+1));
-                port_range(start, end);
-            }else {
-                std::cerr << "Invalid port range: " << range << "\n";
-            }
-        }else {
-            std::cerr << "Unknown argument: " << arg << "\n(Hint: Try \"" << arg[0] << " -h (or --help)\")\n";
+        }
+
+        if(target_address == "not set")
+        {
+            std::cout << "[E-T] Please provide a target address (e.g. -t=192.168.0.1 or -t=www.example.com).\n";
+            return 0;
+        } else if (target_port == 0 && port_range == "0")
+        {
+            std::cout << "[E-P-PR] Please provide a target port (e.g. -p=80) or port range (e.g. -pr=1-100).\n";
+            return 0;
+        } 
+        processTarget(target_address);
+        std::cout << "Target Address: " << target_address << "\n";
+        if(target_port != 0)
+        {
+            std::cout << "Target Port: " << target_port << "\n";
+        } else
+        {
+            std::cout << "Target Port Range: " << port_range << "\n";
         }
     }
-}
 
 
-// To do:
-/*
-[] Port Scanner Functionality
-[] Improved Exception Handling
-[] CLI ARGS Input Validation
-*/
+    // To do:
+    /*
+    [] Port Scanner Functionality
+    [] Improved Exception Handling
+    [] CLI ARGS Input Validation
+    */
